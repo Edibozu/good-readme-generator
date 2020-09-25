bc06520399c5abbe1b568f724726adf1f07f6950
Eduardo Pineda <edibozu@gmail.com>
2020-09-24 22:10:48 -0400

    started writing the sections for the information to go into for the README, including description and the table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 function generateMarkdown(data) {
     return `# ${data.title}
-  
+    ![anything](https://img.shields.io/badge/banana-potato-blue)
+
+    ## Description
+    ${data.description}
+
+    ## Table of Contents
+    *[Installation](#Installation)
+    *[Usage](#Usage)
+    *[Contribute](#Contribute)
+    *[Tests](#Tests)
+    *[Questions](#Questions)
+    *[License](#License)
+    
   `;
   }
   
