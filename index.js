6d286f9b31596cb18136496b45d98cbc009c5224
Eduardo Pineda <edibozu@gmail.com>
2020-09-24 21:48:39 -0400

    created the function to write the readme file and console.logged the user imputs to make sure the code worked properly

--- index.js
+++ index.js
@@ -5,55 +5,61 @@ const generateMarkdown = require(`./utils/generateMarkdown`);
 // array of questions for user
 const questions = [];
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "title",
-    message: "What is the title of your project?",
-  },
-  {
-    type: "inpiut",
-    name: "description",
-    message: "Please provide a description of your project.",
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "Please provide installation instruction for your project.",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "Please provide usage criteria for your project.",
-  },
-  {
-    type: "list",
-    name: "license",
-    message:
-      "Please choose, from the list below, the license you would like to use for your project.",
-    choices: ["MIT", "CPAN", "NPM", "GNU", "APM", "ISC"],
-  },
-  {
-    type: "input",
-    name: "contribute",
-    message:
-      "Please provide how others may go about contributing to this project.",
-  },
-  {
-    type: "input",
-    name: "tests",
-    message: "Please provide how others may test this project.",
-  },
-  {
-    type: "input",
-    name: "questions",
-    message:
-      "Please provide your email address and GitHub username associated with this project.",
-  },
-]);
-
-// function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "inpiut",
+      name: "description",
+      message: "Please provide a description of your project.",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Please provide installation instruction for your project.",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Please provide usage criteria for your project.",
+    },
+    {
+      type: "list",
+      name: "license",
+      message:
+        "Please choose, from the list below, the license you would like to use for your project.",
+      choices: ["MIT", "CPAN", "NPM", "GNU", "APM", "ISC"],
+    },
+    {
+      type: "input",
+      name: "contribute",
+      message:
+        "Please provide how others may go about contributing to this project.",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Please provide how others may test this project.",
+    },
+    {
+      type: "input",
+      name: "questions",
+      message:
+        "Please provide your email address and GitHub username associated with this project.",
+    },
+  ])
+  // function to write README file
+  .then((answers) => {
+    var readMe = generateMarkdown(answers);
+    fs.writeFile("generate.md", readMe, (err) => {
+      if (err) throw err;
+      console.log("Successfully created a README!");
+    });
+  });
 
 // function to initialize program
 function init() {}
